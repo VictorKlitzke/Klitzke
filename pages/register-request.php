37589179884911129401fed97da243dd9ac1df36
Victor Gabriel Miranda Klitzke <71741646+VictorKlitzke@users.noreply.github.com>
2024-02-28 06:52:44 -0400

    Fix: Tela de pedido

--- pages/register-request.php
+++ pages/register-request.php
@@ -71,30 +71,56 @@
         <div class="list">
             <table>
                 <thead>
+
                     <tr>
                         <td>Mesa</td>
                         <td>Status</td>
                         <td>Total</td>
                         <td>Data</td>
                     </tr>
+
                 </thead>
 
                 <?php
-                
-                    $request_dados = Controllers::SelectRequest('request');
 
-                    foreach ($request_dados as $key => $value){               
-                
-                ?>
+                $request_dados = Controllers::SelectRequest('request');
 
-                <tbody>
-                    <tr>
-                        <td><?php echo htmlspecialchars($value['id_table']); ?></td>
-                        <td><?php echo htmlspecialchars($value['STATUS_REQUEST']); ?></td>
-                        <td><?php echo htmlspecialchars($value['total_request']); ?></td>
-                        <td><?php echo htmlspecialchars($value['date_request']); ?></td>
-                    </tr>
-                </tbody>
+                foreach ($request_dados as $key => $value) {
+
+                    ?>
+
+                    <tbody>
+
+                        <tr>
+                            <td>
+                                <?php echo htmlspecialchars($value['id_table']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($value['STATUS_REQUEST']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($value['total_request']); ?>
+                            </td>
+                            <td>
+                                <?php echo htmlspecialchars($value['date_request']); ?>
+                            </td>
+
+                            <td style="display: flex; justify-content: center; gap: 10px; margin: 6px; padding: 6px;">
+                                <div>
+                                    <a class="btn-edit"
+                                        href="<?php echo INCLUDE_PATH ?>edit-clients?id=<?php echo base64_encode($value['id']); ?>">Adicionar mais items</a>
+                                </div>
+                                <div>
+                                    <a class="btn-disable" href="<?php echo INCLUDE_PATH ?>">Faturar</a>
+                                </div>
+                                <div>
+                                    <a actionBtn="delete" class="btn-delete"
+                                        href="<?php echo INCLUDE_PATH ?>list-clients?delete=<?php echo $value['id']; ?>">Detalhes</a>
+                                </div>
+                            </td>
+                        </tr>
+
+                    </tbody>
 
                 <?php } ?>
 
