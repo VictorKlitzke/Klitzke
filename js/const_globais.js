5c38409fec9e84e85fb055c04f5795a852b3af8f
Victor Gabriel Miranda Klitzke <71741646+VictorKlitzke@users.noreply.github.com>
2024-08-05 23:07:12 -0400

    fix: menu atualizado

---
+++ js/const_globais.js
@@ -0,0 +1,5 @@
+const BASE_SEARCH = "http://localhost:3000/Klitzke/controllers/";
+const BASE_URL = "http://localhost:3000/Klitzke/ajax/";
+const BASE_CLASS = "http://localhost:3000/Klitzke/classes/";
+const BASE_CONTROLLERS = "http://localhost:3000/Klitzke/controllers/";
+const BASE_PATH_HOME = "http://localhost:3000/Klitzke/";
--- js/list_system.js
+++ js/list_system.js
@@ -1,8 +1,3 @@
-const BASE_URL = "http://localhost:3000/Klitzke/ajax/";
-const BASE_CLASS = "http://localhost:3000/Klitzke/classes/";
-const BASE_CONTROLLERS = "http://localhost:3000/Klitzke/controllers/";
-const BASE_PATH_HOME = "http://localhost:3000/Klitzke/";
-
 const selectedProducts = [];
 window.onload = ListProducts();
 
